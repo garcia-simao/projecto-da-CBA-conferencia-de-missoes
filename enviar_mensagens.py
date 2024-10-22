86b23864c7b81f1271ac8372feb041608d13aab2
garcia-simao <silviosimons@gmail.com>
2024-10-22 17:22:01 +0100

    atualizacao 1.6

--- enviar_mensagens.py
+++ enviar_mensagens.py
@@ -11,20 +11,28 @@ headers = {
 }
 
 #lendo o arquivo excel
-documento_excel="numeros_de_telefone_teste.ods"
-ler_arquivo= pd.read_excel(documento_excel)
-#ler_arquivo= pd.read_csv(documento_excel)
-
+documento_excel="cimi_final.csv"
+#ler_arquivo= pd.read_excel(documento_excel)
+ler_arquivo= pd.read_csv(documento_excel)
+numeros_processados = set()
 #a primerira coluna do excel deve ter uma descrição e a seguir numeros
 #percorrer o documento excel e enviar mensagem a cada numero.
 for i , j in ler_arquivo.iterrows():
-    telefone = j['numero_telefone']
+    telefone = j['Telefone']
+
 
     # Verificar se a célula está vazia ou se o número contém caracteres não numéricos
     if pd.isna(telefone) or not str(telefone).isdigit() or len(
             str(telefone)) < 9:
         print(f"Número inválido ou célula vazia: {telefone}, ignorando...")
         continue  # Pula para a próxima iteração do loop
+
+    #verificar numeros duplicados
+    if telefone in numeros_processados:
+        print(f"Número duplicado encontrado: {telefone}, ignorando...")
+        continue  # Pula para a próxima iteração do loop
+
+    numeros_processados.add(telefone)
 
     mensagem =( "Amado(a), confirmamos a sua inscricao na Conferencia Internacional de Missoes, dia 26.2"
                 "As contrib. de 1.000kzs podem ser enviadas para o IBAN A006 0040 0000 4387 5824 1066 2"
